bd174e12c4d27c2375dffc6df7fa1602a0e1710e
James Bourbeau <jrbourbeau@gmail.com>
2022-10-12 11:10:39 -0500

    Use arrow options with computing meta

--- dask_snowflake/core.py
+++ dask_snowflake/core.py
@@ -193,7 +193,7 @@ def read_snowflake(
             # This should never since the above check_can_use* calls should
             # raise before if arrow is not properly setup
             raise RuntimeError(f"Received unknown result batch type {type(b)}")
-        meta = b.to_pandas()
+        meta = b.to_pandas(**arrow_options)
         break
 
     if not batches:

ef233fb1f2131ba07fd69b0c7920d29d62d2ba62
fjetter <fjetter@users.noreply.github.com>
2023-11-10 09:14:24 +0100

    fix init

--- dask_snowflake/__init__.py
+++ dask_snowflake/__init__.py
@@ -1,3 +1,8 @@
+from pkg_resources import DistributionNotFound, get_distribution
+
 from .core import read_snowflake, to_snowflake
 
-__version__ = "0.2.0"
+try:
+    __version__ = get_distribution(__name__).version
+except DistributionNotFound:
+    pass
